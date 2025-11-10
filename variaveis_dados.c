fb5b778618426f2cb710001775e59d4d68bcba45
Magno Vieira <luizwermelinger@gmail.com>
2025-11-09 22:17:09 -0300

    Variáveis e tipos de dados
    int => inteiro.
    float => Ponto flutuante, nº decimal, valores menores.
    double => Ponto flutuante, nº decimal, valores maiores.
    char => Armazena caracteres, tipo letras e números

---
+++ variaveis_dados.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade; // int => inteiro
+    int Idade;
+    int IDADE;
+    int idadE;
+    int quantidade = 1;
+    float altura = 1.75; // Ponto flutuante, nº decimal, valores menores
+    double peso = 90.3;  // Ponto flutuante, nº decimal, valores maiores
+    char letra = 'A';    // Armazena caracteres
+    char nome[20] = "João";
+    int _test;
+    int t2;
+    int test_1;
+
+    idade = 25;
+    quantidade = 10;
+
+    /*
+    Nunca usar palavras reservadas tipo : int return, int int etc...
+    */
+
+
+}
