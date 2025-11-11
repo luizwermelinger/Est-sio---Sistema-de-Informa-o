74680c20b5aaf4ff8df37b0aaa1ac8128b3f65bd
Magno Vieira <luizwermelinger@gmail.com>
2025-11-11 13:22:27 -0300

    Técnicas Avançadas em C

---
+++ TecnicasAvançadas.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 25; //= : está sendo inicializada a informação com 25.
+    int quantidade = 1;
+    float altura = 1.75;  //Trabalhar com casas decimais ou seja trabalhar com (virgulas) exp: 1,73.
+    double peso = 90.3;   //Trabalha com pontos flutuantes porám com maiores valores exp: 1,78265.
+    char letra = 'A';    //Armazena caracteres do tipo letras.
+    char nome[35] = "Magno Vieira";     //Armazena strings , armazena 35 caracteres.
+    int numeros[5] = {10, 20, 30, 40, 50}; //Array (vetor), ou seja armazena multiplos valores.
+
+    //idade = 25; poderia ter declarado a variavel tanto nocomeço quanto no meio do código.
+    //quantidade = 10; subistitui a informação inicial ou seja antes era 1 e agora são 10.
+
+
+
+}
